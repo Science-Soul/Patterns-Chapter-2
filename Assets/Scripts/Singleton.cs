c58d6bdb005e00baf76981dc324d446a6238c5c2
Богдан Волошин <voloshinbogdann@gmail.com>
2026-01-01 20:13:27 +0700

    Добавил потокобезопасность в Одиночку

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -2,39 +2,42 @@ using UnityEngine;
 
 public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
+    private static readonly object _threadLock = new object();
+    private static bool _isQuitting = false;
     private static T _instance;
     public static T Instance
     {
         get
         {
+            if (_isQuitting)
+            {
+                return null;
+            }
+
             if (_instance == null)
             {
                 _instance = FindAnyObjectByType<T>();
 
-                if (_instance == null)
+                lock (_threadLock)
                 {
-                    var singletonGO = new GameObject();
-                    singletonGO.name = typeof(T).Name + " Persists";
-                    _instance = singletonGO.AddComponent<T>();
+                    if (_instance == null)
+                    {
+                        var singletonGO = new GameObject();
+                        singletonGO.name = typeof(T).Name + " Persists";
+                        _instance = singletonGO.AddComponent<T>();
 
-                    DontDestroyOnLoad(singletonGO);
-                    Debug.Log("New instance created!");
+                        DontDestroyOnLoad(singletonGO);
+                        Debug.Log("New instance created!");
+                    }
+
                 }
-
             }
             return _instance;
         }
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void OnDestroy()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        _isQuitting = true;
     }
 }
